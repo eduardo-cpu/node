3e7921a9c615acf9a9d957fbb28d563f41a9c382
Eduardo <eduardosantossm@hotmail.com>
2023-03-13 15:33:38 -0300

    aula 1

---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "ola, mundo!";
+const texto2 = 'Ola, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+const citacao = "Meu nome e ";
+const meuNome = "Eduardo";
+
+
+console.log(citacao);
+
+// concatenacao (+)
+
+console.log(citacao+meuNome);
+
+
+//template string Ou template literal
+
