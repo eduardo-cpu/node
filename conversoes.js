16ef06531227f44627cf2e8c0eb81b52a8fe8010
Eduardo <eduardosantossm@hotmail.com>
2023-03-14 14:44:00 -0300

    codigos e conversoes

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado 
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+
+//Number
+//string
+console.log(numero + numeroString)
+
+
+// conversao explicita
+
+
